62f56e8dc3d8017b2eb4383763f6f64336fe2f21
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-08-18 14:40:43 +0530

    Implment UI stage - 1

--- src/main/java/lk/ijse/dep7/controller/ManageCustomersFormController.java
+++ src/main/java/lk/ijse/dep7/controller/ManageCustomersFormController.java
@@ -3,11 +3,14 @@ package lk.ijse.dep7.controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
@@ -26,12 +29,37 @@ public class ManageCustomersFormController {
     public JFXButton btnSave;
     public JFXTextField txtCustomerAddress;
     public TableView<CustomerTM> tblCustomers;
+    public JFXButton btnAddNewCustomer;
 
     public void initialize() {
         tblCustomers.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
         tblCustomers.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
         tblCustomers.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("address"));
 
+        initUI();
+
+        tblCustomers.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            btnDelete.setDisable(newValue == null);
+
+            if (newValue != null){
+                txtCustomerId.setText(newValue.getId());
+                txtCustomerName.setText(newValue.getName());
+                txtCustomerAddress.setText(newValue.getAddress());
+
+                txtCustomerId.setDisable(false);
+                txtCustomerName.setDisable(false);
+                txtCustomerAddress.setDisable(false);
+            }
+        });
+    }
+
+    private void initUI(){
+        txtCustomerId.clear();
+        txtCustomerName.clear();
+        txtCustomerAddress.clear();
+        txtCustomerId.setDisable(true);
+        txtCustomerName.setDisable(true);
+        txtCustomerAddress.setDisable(true);
         txtCustomerId.setEditable(false);
         btnSave.setDisable(true);
         btnDelete.setDisable(true);
@@ -49,26 +77,52 @@ public class ManageCustomersFormController {
     }
 
     public void btnAddNew_OnAction(ActionEvent actionEvent) {
+        txtCustomerId.setDisable(false);
+        txtCustomerName.setDisable(false);
+        txtCustomerAddress.setDisable(false);
         txtCustomerId.clear();
         txtCustomerId.setText(generateNewId());
         txtCustomerName.clear();
         txtCustomerAddress.clear();
         txtCustomerName.requestFocus();
         btnSave.setDisable(false);
+        tblCustomers.getSelectionModel().clearSelection();
     }
 
     public void btnSave_OnAction(ActionEvent actionEvent) {
+        String id = txtCustomerId.getText();
+        String name = txtCustomerName.getText();
+        String address = txtCustomerAddress.getText();
+
+        if (!name.matches("[A-Za-z ]+")){
+            new Alert(Alert.AlertType.ERROR, "Invalid name").show();
+            txtCustomerName.requestFocus();
+            return;
+        }else if (!address.matches(".{3,}")){
+            new Alert(Alert.AlertType.ERROR, "Address should be at least 3 characters long").show();
+            txtCustomerAddress.requestFocus();
+            return;
+        }
+
+        /* Todo: We need to save this in our DB First */
+        tblCustomers.getItems().add(new CustomerTM(id, name, address));
+        btnAddNewCustomer.fire();
     }
 
     public void btnDelete_OnAction(ActionEvent actionEvent) {
+        tblCustomers.getItems().remove(tblCustomers.getSelectionModel().getSelectedItem());
+        tblCustomers.getSelectionModel().clearSelection();
+        initUI();
     }
 
     private String generateNewId(){
         if (tblCustomers.getItems().isEmpty()){
             return "C001";
         }else{
-            /* Todo: generate new id when there are customers */
-            throw new UnsupportedOperationException("Yet to implement");
+            String id = tblCustomers.getItems().get(tblCustomers.getItems().size() - 1).getId();
+            int newCustomerId = Integer.parseInt(id.replace("C", "")) + 1;
+            return String.format("C%03d", newCustomerId);
         }
     }
+
 }
---
+++ src/test/java/lk/ijse/dep7/controller/ManageCustomersFormControllerTest.java
@@ -0,0 +1,19 @@
+package lk.ijse.dep7.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManageCustomersFormControllerTest {
+
+    @Test
+    void generateNewId2() {
+        ManageCustomersFormController ctrl = new ManageCustomersFormController();
+//        assertEquals(ctrl.generateNewId2("C001"), "C002");
+//        assertEquals(ctrl.generateNewId2("C005"), "C006");
+//        assertEquals(ctrl.generateNewId2("C010"), "C011");
+//        assertEquals(ctrl.generateNewId2("C100"), "C101");
+//        assertEquals(ctrl.generateNewId2("C1000"), "C1001");
+//        assertEquals(ctrl.generateNewId2("C10000"), "C10001");
+    }
+}

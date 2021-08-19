0e6ee07fa9b988373609dab620fe18d3ba5b29f4
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-08-19 10:16:43 +0530

    Add CustomerService and CustomerDTO

---
+++ src/main/java/lk/ijse/dep7/service/CustomerService.java
@@ -0,0 +1,29 @@
+package lk.ijse.dep7.service;
+
+import lk.ijse.dep7.dto.CustomerDTO;
+
+import java.util.List;
+
+public class CustomerService {
+
+    public void saveCustomer(CustomerDTO customer){
+
+    }
+
+    public void updateCustomer(CustomerDTO customer) {
+
+    }
+
+    public void deleteCustomer(String id) {
+
+    }
+
+    public CustomerDTO findCustomer(String id) {
+        return null;
+    }
+
+    public List<CustomerDTO> findAllCustomers() {
+        return null;
+    }
+
+}

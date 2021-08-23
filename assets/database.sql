97183b1ab848b91bbe8d7c908c49806298ad3489
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-08-23 12:26:16 +0530

    Update the DB Script and ERD

--- assets/database.sql
+++ assets/database.sql
@@ -12,3 +12,22 @@ CREATE TABLE item
     unit_price  DECIMAL      NOT NULL,
     qty_on_hand INT          NOT NULL
 );
+
+CREATE TABLE `order`
+(
+    id          VARCHAR(10) PRIMARY KEY,
+    date        DATE       NOT NULL,
+    customer_id VARCHAR(8) NOT NULL,
+    CONSTRAINT FOREIGN KEY (customer_id) REFERENCES customer (id)
+);
+
+CREATE TABLE order_detail
+(
+    order_id   VARCHAR(10) NOT NULL,
+    item_code  VARCHAR(10) NOT NULL,
+    unit_price DECIMAL     NOT NULL,
+    qty        INT         NOT NULL,
+    CONSTRAINT PRIMARY KEY (order_id, item_code),
+    CONSTRAINT FOREIGN KEY (order_id) REFERENCES `order` (id),
+    CONSTRAINT FOREIGN KEY (item_code) REFERENCES item (code)
+);

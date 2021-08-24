78b7d09015bf3598a50cc1fea0ed555f8e3d45fa
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-08-24 12:03:17 +0530

    Add view order form

---
+++ src/main/java/lk/ijse/dep7/controller/ViewOrderFormController.java
@@ -0,0 +1,16 @@
+package lk.ijse.dep7.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
+public class ViewOrderFormController {
+    public AnchorPane root;
+    public JFXTextField txtCustomerName;
+    public TableView tblOrderDetails;
+    public Label lblDate;
+    public Label lblId;
+    public Label lblTotal;
+    public JFXTextField txtCustomerID;
+}
